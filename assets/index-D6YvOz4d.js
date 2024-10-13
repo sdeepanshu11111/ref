394a57a583d9c1697a80d3bc8d3467b9371879eb
Deepanshu <deepanshu.sharma@letx.co>
2024-10-13 21:02:32 +0530

    Updates

---
+++ assets/index-D6YvOz4d.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/index.umd-CAdTEReZ.js","assets/index-C8NQHwyi.js","assets/index-CYfWBmvb.css"])))=>i.map(i=>d[i]);
+import{an as n,ao as a,aG as o}from"./index-C8NQHwyi.js";function u(i){return n(this,void 0,void 0,function(){var t;return a(this,function(e){switch(e.label){case 0:return[4,o(()=>import("./index.umd-CAdTEReZ.js").then(r=>r.i),__vite__mapDeps([0,1,2]))];case 1:return t=e.sent(),i._plugins=t,[2]}})})}export{u as loadLegacyVideoPlugins};
---
+++ assets/invalidPath-CN3kR3a_.js
@@ -0,0 +1,1 @@
+import{N as t,Q as s}from"./index-C8NQHwyi.js";import{R as o}from"./index-D7LbYwfl.js";const r=e=>t.jsx(o,{status:"404",title:"404",subTitle:"Sorry, the page you visited does not exist.",extra:t.jsx(s,{onClick:()=>e.navigate("/login"),type:"primary",children:"Back Home"})});export{r as default};
